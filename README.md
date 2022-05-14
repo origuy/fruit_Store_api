3e3323c5b7877bbff31449b95e6cf51408ac14c5
Ori Guy <origuy2021@gmail.com>
2022-05-14 11:55:51 +0300

    final project

---
+++ README.md
@@ -0,0 +1,18 @@
+
+![Logo](assets\Ori Logo.png)
+
+
+# Arranging products - API Assiment
+
+I built an API to simulate the connection with the server.
+the server contains two files
+
+the product file - contains all the products in the store + their attributes
+the discount file - contains the data about the discount at the store
+
+the project goals are:
+  1. get the data from the local server (fetch from JSON file).
+  2. show the products to the user
+  3. with the click of a button the user will be able to see if there is a discount
+
+
--- javaScript/app.js
+++ javaScript/app.js
@@ -11,67 +11,81 @@ fetch("/products.json")
 	.then((data) => drawSite(data));
 
 let drawSite = (obj) => {
+	for (let i = 0; i < obj.products.length; i++) {
+		//        cardContainer.innerHTML += ` <div class="card">
+		//        <img class="productImg" src="" alt="">
+		//        <h3 class="productTitle"></h3>
+		//        <p class="productPrice"></p>
+		//        <button class="btn" type="submit">בדוק הנחה</button>
+		//    </div>`
 
-    for (let i = 0; i < obj.products.length; i++) {
-       cardContainer.innerHTML += ` <div class="card">
-       <img class="productImg" src="" alt="">
-       <h3 class="productTitle"></h3>
-       <p class="productPrice"></p>
-       <button class="btn" type="submit">בדוק הנחה</button>
-   </div>`
-        
-    }
-    productImg = document.querySelectorAll(".productImg");
+		let img = document.createElement("img");
+		img.className = "productImg";
+		let h3 = document.createElement("h3");
+		h3.className = "productTitle";
+		let p = document.createElement("p");
+		p.className = "productPrice";
+		let btn = document.createElement("button");
+		btn.className = "btn";
+        btn.innerText = "בדוק הנחה"
+		let card = document.createElement("div");
+		card.className = "card";
+		let containerArr = [img, h3, p, btn];
+		containerArr.forEach((element) => {
+			card.appendChild(element);
+		});
+		cardContainer.appendChild(card);
+	}
+	productImg = document.querySelectorAll(".productImg");
 	productTitle = document.querySelectorAll(".productTitle");
 	productPrice = document.querySelectorAll(".productPrice");
 	productBtn = document.querySelectorAll(".btn");
-    let count = 0;
+	let count = 0;
 	obj.products.forEach((data) => {
-            for (const [key, value] of Object.entries(data)) {
-                switch (key) {
-                
-                    case "img":
-                        productImg[count].src = value;
-                        break;
-                    case "name":
-                        productTitle[count].innerHTML = value;
-                        break;
-                    case "price":
-                        productPrice[count].innerHTML = value;
-                        break;
-        
-                    default:
-                        "";
-                        break;
-                }
-              }
-              count++
-              
-     
-        
-	});
-    setTimeout(() => {
-        cardContainer.style.opacity = 1;
+	        for (const [key, value] of Object.entries(data)) {
+	            switch (key) {
 
-    }, 500);
+	                case "img":
+	                    productImg[count].src = value;
+	                    break;
+	                case "name":
+	                    productTitle[count].innerHTML = value;
+	                    break;
+	                case "price":
+	                    productPrice[count].innerHTML = value;
+	                    break;
+
+	                default:
+	                    "";
+	                    break;
+	            }
+	          }
+	          count++
+
+	});
+	setTimeout(() => {
+	    cardContainer.style.opacity = 1;
+
+	}, 500);
 };
 
-document.addEventListener( "click", btnClicked);
-let message = (json,evt) => {
-    console.log(json)
-    if(evt.path[1].innerHTML.includes("אבטיח")){
-        alert(`יש לנו הנחה של ${json.watermelon} על המוצר! `)
-    }else if(evt.path[1].innerHTML.includes("תפוח עץ")){
-        alert(`יש לנו הנחה של ${json.apple} על המוצר! `)
-    }else{
-        alert(`אין לנו היום הנחה על המוצר הזה, אבל מי יודע מה יהיה מחר 😃 `)
-    }
+document.addEventListener("click", btnClicked);
+let message = (json, evt) => {
+	console.log(json);
+	if (evt.path[1].innerHTML.includes("אבטיח")) {
+		alert(`יש לנו הנחה של ${json.watermelon} על המוצר! `);
+	} else if (evt.path[1].innerHTML.includes("תפוח עץ")) {
+		alert(`יש לנו הנחה של ${json.apple} על המוצר! `);
+	} else {
+		alert(`אין לנו היום הנחה על המוצר הזה, אבל מי יודע מה יהיה מחר 😃 `);
+	}
+};
+function btnClicked(event) {
+	let element = event.target;
+	let json;
+	if (element.classList[0] === "btn") {
+		fetch("/discount.json")
+			.then((response) => response.json())
+			.then((data) => message(data, event));
+	}
 }
-function btnClicked(event){
-    let element = event.target;
-    let json ;
-    if(element.classList[0] === 'btn'){
-        fetch('/discount.json').then(response => response.json()).then(data => message(data,event))
-    }
-}
-
